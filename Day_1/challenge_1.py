69fd3c97b2553b50c8379ea67a360f53bb0b9600
Sebastian Hozák <hozaksebastian@gmail.com>
2020-11-06 11:30:56 +0100

    Solving the 1. day challenge

---
+++ Day_1/challenge_1.py
@@ -0,0 +1,4 @@
+print("Hello user, welcome to the Band name Generator")
+user_input = input("What's the name of the city you grew up in: \n ")
+pet_name = input("What's the name of your pet: \n ")
+print(f"Your band name could be {pet_name} {user_input}")
