7b82e4f3578c5105d09a1f4c4cb32891ed6e369d
Sebastian Hozák <hozaksebastian@gmail.com>
2020-11-09 23:16:10 +0100

    Exercises - day 9

---
+++ Beginner/Day 9/nesting.py
@@ -0,0 +1,22 @@
+# Nesting Dictionary in a Dictionary
+
+travel_log = {
+	"France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+	"Germany": ["Berlin", "Hamburg", "Stuttagart"]
+}
+
+
+# Nesting Dictionary in a List
+
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
